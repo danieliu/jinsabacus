6134279bad7a72458ca07b6a5cf3aa0ca5db0058
Daniel Liu <dnldrink@gmail.com>
2016-01-13 14:53:18 -0800

    Post image hover, uncategorized colors, cleanup cat-specific css

--- templates/content.php
+++ templates/content.php
@@ -1,12 +1,12 @@
-<div class="col-sm-6 col-lg-4 news-item">
-	<a href="<?php the_permalink(); ?>">
+<div class="col-sm-6 col-lg-4 news-item <?php echo strtolower(esc_html( get_the_category()[0]->name )); ?>">
+	<a class="post-featured" href="<?php the_permalink(); ?>">
 		<div class="post-image" style="background-image:url('<?php if ( has_post_thumbnail() ) {
 				the_post_thumbnail_url('custom-thumb');
 			} else {
 				echo bloginfo('template_directory') . "/dist/images/default-post-image.jpg";
 			} ?>')"></div>
 	</a>
-	<div class="post-content <?php echo strtolower(esc_html( get_the_category()[0]->name )); ?>">
+	<div class="post-content">
 		<div class="post-category"><?php echo esc_html( get_the_category()[0]->name ); ?></div>
 		<h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 		<p class="post-meta"><?php the_time( 'F j, Y' ) ?></p>
